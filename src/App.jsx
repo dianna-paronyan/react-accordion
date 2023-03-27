483c38328170e51e7e3d4902673595210b04c8fe
dianna1234 <01234dianna@gmail.com>
2023-03-27 23:58:25 +0400

    create accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
 
-function App() {
+export default class App extends Component {
+  state = {countries:[],isActive:false}
+  componentDidMount(){
+    fetch('https://countriesnow.space/api/v0.1/countries/capital').then(res=>res.json()).then((res)=>{
+      console.log(res);
+      this.setState({countries:res.data})
+    })
+  }
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+  changeIsActive= (id)=>{
+    if(id === this.state.isActive){
+
+      this.setState({isActive:false});
+      return;
+    }
+    this.setState({isActive:id})
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.countries.map((country)=>{
+          return(
+            <div>
+              <p>{country.name}</p>
+              <span onClick={()=>this.changeIsActive(country.iso2)}>{this.state.isActive === country.iso2 ? '-' : '+'}</span>
+              {this.state.isActive === country.iso2 && <p>{country.capital}</p>}
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
-export default App
