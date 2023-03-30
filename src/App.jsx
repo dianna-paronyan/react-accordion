e795bf2dc5e4ebdf7ef3fe767e8d9f3d5f6c3324
dianna1234 <01234dianna@gmail.com>
2023-03-30 22:38:31 +0400

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Countries from './Countries'
 import CountriresFuncComp from './CountriresFuncComp'
+import Home from './Home'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Countries />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/countries' element={<Countries />} />
       <Route path='/countries/:page' element={<CountriresFuncComp />} />
     </Routes>
   )
